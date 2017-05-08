258cef5cb0f19d1751243315e8b094c581d78996
Andy Georges <itkovian@gmail.com>
2017-05-08 11:58:49 +0200

    Add program info for lmod

--- files/lmod.rb
+++ files/lmod.rb
@@ -3,6 +3,6 @@ version=2
 type=@lmod_info:username=%username:char-to{"extradata":","}%, cluster=%cluster:char-to{"extradata": ","}%, jobid=%jobid:char-sep{"extradata": ","}%
 type=@lmod_module:module=%name:char-to{"extradata": "/"}%/%version:char-to{"extradata": ","}%
 
-rule=lmod_load:lmod::  %info:@lmod_info%, userload=%userload:char-to{"extradata": ","}%, %module:@lmod_module%, fn=%filename:word%
+rule=lmod_load:%program:literal{"text": "lmod"}%::  %info:@lmod_info%, userload=%userload:char-to{"extradata": ","}%, %module:@lmod_module%, fn=%filename:word%
 
-rule=lmod_command:lmod::  %info:@lmod_info%, cmd=%command:char-to{"extradata": ","}%, args=%arguments:word%
+rule=lmod_command:%program:literal{"text": "lmod"}%::  %info:@lmod_info%, cmd=%command:char-to{"extradata": ","}%, args=%arguments:word%
